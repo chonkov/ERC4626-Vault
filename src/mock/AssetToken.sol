63415ef2a508c01b155c41cc6cce8e3dc3089650
grc02 <georgi.chonkov27@gmail.com>
2023-11-15 19:39:39 +0200

    Made minor changes to mock tokens

--- src/mock/AssetToken.sol
+++ src/mock/AssetToken.sol
@@ -4,5 +4,7 @@ pragma solidity 0.8.21;
 import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract AssetToken is ERC20 {
-    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {}
+    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {
+        _mint(_msgSender(), 10_000 ether);
+    }
 }
--- src/mock/YieldToken.sol
+++ src/mock/YieldToken.sol
@@ -5,5 +5,7 @@ import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol"
 import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract YieldToken is Ownable, ERC20 {
-    constructor(string memory name_, string memory symbol_) Ownable(_msgSender()) ERC20(name_, symbol_) {}
+    constructor(string memory name_, string memory symbol_) Ownable(_msgSender()) ERC20(name_, symbol_) {
+        _mint(_msgSender(), 10_000 ether);
+    }
 }
